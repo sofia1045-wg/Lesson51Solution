596b7cfa3ef8058d60e9ae863a1fb3d83cf20791
Sofia Chernysh <lenovotab2020new@gmail.com>
2026-05-03 18:18:58 +0300

    add

--- Task01/logic.cpp
+++ Task01/logic.cpp
@@ -7,3 +7,22 @@
 // Дополнительно в функции необходимо предусмотреть механизм "защиты от дурака"
 // для работы с некорректными данными (в данном случае функция должна 
 // возвращать значение -1).
+
+#include "logic.h"
+
+int count_nonzero_elements(int array[], int size) {
+	if (size <= 0) {
+		return -1;
+	}
+
+	int count = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] != 0) {
+			count++;
+		}
+	}
+
+	return count;
+}
